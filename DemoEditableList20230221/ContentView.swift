56ed89fa7018294ab2c447213546657e3af9f29b
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-21 21:12:35 -0500

    Initial Commit

---
+++ DemoEditableList20230221/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DemoEditableList20230221
+//
+//  Created by Xavier on 2/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoEditableList20230221/DemoEditableList20230221App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoEditableList20230221App.swift
+//  DemoEditableList20230221
+//
+//  Created by Xavier on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoEditableList20230221App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
